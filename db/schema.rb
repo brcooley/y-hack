cbb892f0d0db130e78263faf81ea24405c140443
Evan Wang <evan@impermium.com>
2013-07-27 09:41:39 +0000

    Updated the schema for the task object

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130727064257) do
+ActiveRecord::Schema.define(:version => 20130727093538) do
 
   create_table "tasks", :force => true do |t|
     t.string   "description"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20130727064257) do
     t.integer  "user_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.string   "tag"
   end
 
   add_index "tasks", ["user_id"], :name => "index_tasks_on_user_id"
