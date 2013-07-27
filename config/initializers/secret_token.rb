6f2741845dd831f77bd515710b237f6cc4ffdec6
Evan Wang <evan@impermium.com>
2013-07-27 01:54:03 +0000

    Initialized empty project, added bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YHack::Application.config.secret_token = '1250183066853d117b786d5026e267c5b3ca94a178aa2cb145a70a9596f57d9b9737b2ab73f11209a30bc6de01aa51f5dfd8b58cbf746acbee3a5cd21d59b065'
