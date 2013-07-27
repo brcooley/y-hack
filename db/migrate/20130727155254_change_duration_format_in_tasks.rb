f4541ad1edb64c36fa3c1a33c31b53ca6e444636
Evan Wang <evan@impermium.com>
2013-07-27 15:55:53 +0000

    added migration for duration -> integer

---
+++ db/migrate/20130727155254_change_duration_format_in_tasks.rb
@@ -0,0 +1,9 @@
+class ChangeDurationFormatInTasks < ActiveRecord::Migration
+  def self.up
+    change_column :tasks, :duration, :integer
+  end
+
+  def self.down
+    change_column :tasks, :duration, :time
+  end
+end
