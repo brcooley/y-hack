21b1765aa85b88219b4c12753cd2178734bc539d
Evan Wang <evan@impermium.com>
2013-07-27 07:15:15 +0000

    Create a new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,14 @@
 class TasksController < ApplicationController
+
+  def index
+  end
+
+  def create
+    current_user.tasks.create
+    @tasks = current_user.tasks
+    respond_to do |format|
+      format.js
+    end
+  end
+
 end
